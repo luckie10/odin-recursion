6f626657581c721e1d61761256b5ea40ceed5229
Boban Kilvin <btrajcevski10@gmail.com>
2023-07-14 22:24:40 -0400

    Add mergeSort solution

---
+++ mergesort/mergesort.js
@@ -0,0 +1,26 @@
+const merge = (left, right) => {
+  let i = 0;
+  let j = 0;
+  let arr = [];
+
+  while (i < left.length && j < right.length) {
+    left[i] < right[j] ? arr.push(left[i++]) : arr.push(right[j++]);
+  }
+
+  while (i < left.length) arr.push(left[i++]);
+  while (j < right.length) arr.push(right[j++]);
+
+  return arr;
+};
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const left = mergeSort(arr.splice(0, arr.length / 2));
+  const right = mergeSort(arr.splice(0));
+
+  return merge(left, right);
+};
+
+console.log(mergeSort([]));
+console.log(mergeSort([7, 4, 1, 3, 8, 6, 9, 0, 2, 5]));
